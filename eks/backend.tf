7169f30ef95083003a8c63ca972022f09e7cfcb5
roammate-in <parag@roammate.in>
2025-01-13 14:01:37 +0530

    changing region

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "my-aws-jenkins-baket01"
-    region         = "us-east-1"
+    region         = "eu-north-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
