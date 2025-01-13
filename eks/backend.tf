34f29c5ce98ef6b412485525d212aa73ee380597
roammate-in <parag@roammate.in>
2025-01-13 13:45:19 +0530

    adding configuration

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "my-aws-jenkins-baket01"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
